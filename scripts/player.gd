56f2f04510cec62fc7f627732190233184a0b04a
Joshs Computer <jminchew97@gmail.com>
2025-01-15 18:10:00 -0500

    added auto tile layering function, can now just create a level on one layer using white and black tiles and the function will sort each to their corresponding layer based on the custom layer data for each tile

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,17 +4,26 @@ extends CharacterBody2D
 @export var SPEED = 100.0
 @export var JUMP_VELOCITY = -100.0
 
+@onready var coyote_timer: Timer = $CoyoteTimer
 
+var can_jump = true
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-	if Input.is_action_just_pressed("ui_accept") :
+	if Input.is_action_just_pressed("ui_accept") and can_jump:
 		velocity.y = JUMP_VELOCITY
+		
+	# if player is not on floor start coyote timer
+	if is_on_floor() == false:
+		coyote_timer.start()
+		
+	if is_on_floor():
+		can_jump = true
+	#if Input.is_action_just_pressed("ui_accept") :
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,3 +34,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout() -> void:
+	can_jump = false
+	print("cant jump")
+	pass # Replace with function body.
--- scripts/tile_map.gd
+++ scripts/tile_map.gd
@@ -4,24 +4,41 @@ extends TileMap
 @onready var timer: Timer = $Timer
 
 
-enum Layer {Black, White}
+enum Layer {White, Black}
 var current_layer = Layer.Black
 var previous_layer = Layer.White
 var current_layer_modulate_color
-func _ready() -> void:
-	#turn off white later collision
-	tile_set.set_physics_layer_collision_layer(Layer.Black, 1)
-	tile_set.set_physics_layer_collision_layer(Layer.White, 1)
-	pass
 
-func _on_timer_timeout() -> void:
+
+#settings
+@export_category("Settings")
+@export var flicker_rate = 1.0
+@export var flicker = true
+@export var white_only = false
+@export var black_only = false
+
+
+func _ready() -> void:
+	put_tiles_on_correct_layer()
 	
+	tile_set.set_physics_layer_collision_layer(Layer.White, 1)
+	tile_set.set_physics_layer_collision_layer(Layer.Black, 1)
+	if white_only:
+		flicker = false
+		tile_set.set_physics_layer_collision_layer(Layer.Black, 0)
+		set_layer_modulate(Layer.Black, Color(255,255,255,0.075))
+	if black_only:
+		flicker = false
+		tile_set.set_physics_layer_collision_layer(Layer.White, 0)
+		set_layer_modulate(Layer.White, Color(255,255,255,0.075))
+	
+func _on_timer_timeout() -> void:
+	if not flicker:
+		return
 	#print(tile_set.get_physics_layer_collision_layer(Layer.White))
 	match current_layer:
 		
 		Layer.Black:
-			print("On Layer Black!")
-			
 			# make the previous White layer transparent
 			set_layer_modulate(previous_layer, Color(255,255,255,0.075))
 			# make the current Black layer solid colored
@@ -31,12 +48,11 @@ func _on_timer_timeout() -> void:
 			#turn_off_previous_layer_physics()
 			if tile_set.get_physics_layer_collision_layer(Layer.White) == 1:
 				tile_set.set_physics_layer_collision_layer(Layer.White, 0)
-				print("turned off white layer collision")
+
 			# turn ON current layer physics if NOT ON already
 			if tile_set.get_physics_layer_collision_layer(Layer.Black) == 0:
 				tile_set.set_physics_layer_collision_layer(Layer.Black, 1)
-				print("turned on black layer collision")
-			print("-------------------")
+
 			# turn ON current BLACK layer physics
 			
 			#set current layer to White for next iteration
@@ -45,20 +61,17 @@ func _on_timer_timeout() -> void:
 			previous_layer = Layer.Black
 			
 		Layer.White:
-			print("On Layer White!")
 			set_layer_modulate(previous_layer, Color(255,255,255,0.075))
 			set_layer_modulate(current_layer, Color(255,255,255,1.0))
 			
 			#turn_off_previous_layer_physics()
 			if tile_set.get_physics_layer_collision_layer(Layer.Black) == 1:
-				
 				tile_set.set_physics_layer_collision_layer(Layer.Black, 0)
-				print("turned off black layer collision")
+
 			# turn ON current layer physics if NOT ON already
 			if tile_set.get_physics_layer_collision_layer(Layer.White) == 0:
 				tile_set.set_physics_layer_collision_layer(Layer.White, 1)
-				print("turned on white layer collision")
-			print("-------------------")
+
 			
 			#set current layer to Black for next iteration
 			current_layer = Layer.Black
@@ -72,3 +85,16 @@ func _on_timer_timeout() -> void:
 ##func turn_on_current_layer_physics():
 	##if tile_set.get_physics_layer_collision_layer(current_layer) == 0:
 				##tile_set.set_physics_layer_collision_layer(current_layer, 1)
+func put_tiles_on_correct_layer():
+	var main_level_layer = 2
+	var all_used_tiles = get_used_cells(main_level_layer)
+	for tile_coord in all_used_tiles:
+		var tile = get_cell_tile_data(main_level_layer,tile_coord)
+		#if black put on black layer
+		if tile.get_custom_data("layer") == Layer.Black:
+			set_cell(Layer.Black,tile_coord, 0, Vector2i(0,0))
+			set_cell(main_level_layer,tile_coord,-1)
+		if tile.get_custom_data("layer") == Layer.White:
+			set_cell(Layer.White,tile_coord, 0, Vector2i(1,0))
+			set_cell(main_level_layer,tile_coord,-1)
+	
